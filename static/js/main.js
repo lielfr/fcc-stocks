7a60c8ec5281726847eb4bb9eaac02d1be1716b0
lielfr <lielft@gmail.com>
2016-08-28 23:00:37 +0300

    Began rewriting the client-side JS in order to allow dynamically changing the stocks displayed.

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,55 @@
 'use strict';
 
-$(document).ready(function () {
-  $.getJSON('/api/AAPL,GOOGL', function (data) {
+var updateStocksChart = function updateStocksChart(stockNames) {
+  $.getJSON('/api/' + stockNames.join(','), function (data) {
     var stocksChart = new Chart($('#stocksChart'), data);
   });
+};
+
+var StockBox = React.createClass({
+  displayName: 'StockBox',
+
+  render: function render() {
+    return React.createElement(
+      'div',
+      { 'class': 'stockBox' },
+      this.props.name,
+      ' ',
+      React.createElement(
+        'button',
+        { className: 'btn btn-danger', 'aria-label': 'Remove this stock', onClick: this.props.removeItem },
+        React.createElement('i', { className: 'fa fa-times', 'aria-hidden': 'true' })
+      )
+    );
+  }
+});
+
+var BoxesHolder = React.createClass({
+  displayName: 'BoxesHolder',
+
+  getInitialState: function getInitialState() {
+    return {
+      stocks: ['AAPL', 'GOOGL']
+    };
+  },
+  removeItem: function removeItem(event) {
+    var targetBox = event.target.parentElement;
+    targetBox.parentElement.removeChild(targetBox);
+  },
+  render: function render() {
+    var _this = this;
+
+    var blocks = this.state.stocks.map(function (item) {
+      return React.createElement(StockBox, { name: item, removeItem: _this.removeItem });
+    });
+    return React.createElement(
+      'div',
+      { 'class': 'boxesHolder' },
+      blocks
+    );
+  }
+});
+
+$(document).ready(function () {
+  ReactDOM.render(React.createElement(BoxesHolder, null), document.querySelector('#barHolder'));
 });
--- static_src/jsx/main.js
+++ static_src/jsx/main.js
@@ -1,6 +1,44 @@
-
-$(document).ready(() => {
-  $.getJSON('/api/AAPL,GOOGL', (data) => {
+var updateStocksChart = (stockNames) => {
+  $.getJSON('/api/' + stockNames.join(','), (data) => {
     var stocksChart = new Chart($('#stocksChart'), data);
   });
+};
+
+var StockBox = React.createClass({
+  render: function() {
+    return (
+      <div class="stockBox">
+        {this.props.name}
+        &nbsp;
+        <button className="btn btn-danger" aria-label="Remove this stock" onClick={this.props.removeItem}>
+          <i className="fa fa-times" aria-hidden="true"></i>
+        </button>
+      </div>
+    );
+  }
+});
+
+var BoxesHolder = React.createClass({
+  getInitialState: () => ({
+    stocks: ['AAPL', 'GOOGL']
+  }),
+  removeItem: (event) => {
+    let targetBox = event.target.parentElement;
+    targetBox.parentElement.removeChild(targetBox);
+  },
+  render: function() {
+    var blocks = this.state.stocks.map((item) => <StockBox name={item} removeItem={this.removeItem} />);
+    return (
+      <div class="boxesHolder">
+        {blocks}
+      </div>
+    );
+  }
+});
+
+$(document).ready(() => {
+  ReactDOM.render(
+    <BoxesHolder />,
+    document.querySelector('#barHolder')
+  );
 });
